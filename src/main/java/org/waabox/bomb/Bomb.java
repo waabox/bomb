b8ce0d518c8c1481223c208e7dedbafcd30e84f3
waabox <emi@avenida.com>
2016-06-06 17:47:35 -0300

    Using unsafe instead of Objenesis strategy.

--- src/main/java/org/waabox/bomb/Bomb.java
+++ src/main/java/org/waabox/bomb/Bomb.java
@@ -1,5 +1,6 @@
 package org.waabox.bomb;
 
+import java.lang.reflect.Constructor;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,6 +9,8 @@ import org.objenesis.ObjenesisHelper;
 import net.sf.cglib.proxy.Callback;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
+
+import sun.misc.Unsafe;
 
 /** The bomb holds a list of matching expression that works as a bomb
  * detonator, if the proxies object matches with any of the given detonator,
@@ -22,6 +25,21 @@ public class Bomb<T> {
 
   /** The list of expressions, it's never null. */
   private final Set<String> expressions = new HashSet<>();
+
+  /** The unsafe instance.*/
+  private static Unsafe unsafe;
+
+  /** This constructor creates a new Unsafe instance. If cannot create a new
+   * one leave the Bomb#unsafe with null value.
+   */
+  static {
+    try {
+      Constructor<Unsafe> constructor = Unsafe.class.getDeclaredConstructor();
+      constructor.setAccessible(true);
+      unsafe = constructor.newInstance();
+    } catch (Exception ex) {
+    }
+  }
 
   /** Creates a new instance of the {@link Bomb}.
    * @param theInstance the instance to be proxied.
@@ -58,7 +76,6 @@ public class Bomb<T> {
   /** Retrieves the Proxy.
    * @return the proxy, never null.
    */
-  @SuppressWarnings("unchecked")
   public T get() {
     return (T) createProxy(
         instance.getClass(),
@@ -67,18 +84,58 @@ public class Bomb<T> {
 
 
   /** Creates a new proxy.
+   *
    * @param klass the class.
-   * @param interceptor the interceptor.
+   * @param methodInterceptor the interceptor.
    * @return the proxy instance, never null.
    */
-  @SuppressWarnings("unchecked")
-  private static <T> T createProxy(
-      final Class<T> klass, final MethodInterceptor interceptor) {
+  private T createProxy(
+      final Class<?> klass,
+      final MethodInterceptor methodInterceptor) {
+
     Enhancer enhancer = new Enhancer();
     enhancer.setSuperclass(klass);
-    enhancer.setCallbackType(interceptor.getClass());
+    enhancer.setCallbackType(methodInterceptor.getClass());
+
     Class<?> proxyClass = enhancer.createClass();
-    Enhancer.registerCallbacks(proxyClass, new Callback[] { interceptor });
+
+    Enhancer.registerCallbacks(
+        proxyClass, new Callback[] { methodInterceptor }
+    );
+
+    // from unsafe it's fasssssster than Objenesis...
+    T proxy = fromUnsafe(proxyClass);
+
+    if (proxy == null) {
+      // Ok, we are running within a picky jvm.. we'll use Objenesis...
+      proxy = fromObjenesis(proxyClass);
+    }
+
+    return proxy;
+  }
+
+  /** Creates a new proxy instance using {@link Unsafe}.
+   * @param proxyClass the proxy class.
+   * @return the instance, or null.
+   */
+  @SuppressWarnings("unchecked")
+  private T fromUnsafe(final Class<?> proxyClass) {
+    if (unsafe == null) {
+      return null;
+    }
+    try {
+      return (T) unsafe.allocateInstance(proxyClass);
+    } catch (InstantiationException e) {
+      return null;
+    }
+  }
+
+  /** Creates a new instance usign {@link ObjenesisHelper}.
+   * @param proxyClass the proxy class.
+   * @return the instance.
+   */
+  @SuppressWarnings("unchecked")
+  private T fromObjenesis(final Class<?> proxyClass) {
     return (T) ObjenesisHelper.newInstance(proxyClass);
   }
 
